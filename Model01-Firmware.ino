047c170a94b63bcc0449abd989efa1dc3428e82c
Siew Yi Liang <admin@sonictk.com>
2018-08-28 02:06:51 -0700

    binded arrow keys to esdf instead of wasd

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -251,10 +251,10 @@ KEYMAPS(
    ___),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,                    Key_F2,          Key_F3,          Key_F4,        Key_F5,           Key_CapsLock,
-   Key_Tab,  ___,                       Key_UpArrow,     ___,             ___,            ___,             Key_Tab,
-   Key_CapsLock, Key_LeftArrow,         Key_DownArrow,   Key_RightArrow,  ___,            ___,
-   Key_RightControl,  Key_PrintScreen,  Key_Insert,      ___,             ___,            ___,              ___,
+  (___,                        Key_F1,                    Key_F2,             Key_F3,             Key_F4,                 Key_F5,          Key_CapsLock,
+   Key_Tab,                    ___,                       ___,                Key_UpArrow,        ___,                    ___,             Key_Tab,
+   Key_CapsLock,               ___,                       Key_LeftArrow,      Key_DownArrow,      Key_RightArrow,         ___,             
+   Key_RightControl,           Key_PrintScreen,           Key_Insert,         ___,                ___,                    ___,             ___,
    ___, Key_Enter, Key_LeftAlt, ___,
    ___,
 
